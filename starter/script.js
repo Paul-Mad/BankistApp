e0b22fe540a648909a80715fddce2e4907228d32
Paul-Mad <paulin.mad@hotmail.com>
2021-01-14 22:19:23 +0000

    Functions added: displayMovements, calcDisplayBalance, createUsernames

--- starter/script.js
+++ starter/script.js
@@ -38,6 +38,7 @@ const account4 = {
 };
 
 const accounts = [account1, account2, account3, account4];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -65,11 +66,58 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//DISLAY MOVEMENTS
+const displayMovements = function (movements) {
+  //clean the element movements
+  containerMovements.innerHTML = '';
+  //loop into the movements array
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+//Add movement rows  into the movement element
+displayMovements(account1.movements);
+
+//PRINT the balance
+const calcDisplayBalance = function (movement) {
+  const balance = movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${balance}€`;
+};
+
+calcDisplayBalance(account1.movements);
+
+//CREATE username
+const createUsernames = function (accs) {
+  // get the accounts array
+  accs.forEach(function (acc) {
+    // loop into the accoust array
+    acc.username = acc.owner // create a new attribute "username" into the each account inside the accounts array
+      .toLowerCase() // set the owner name to lower case
+      .split() //split the owners name
+      .map(name => name[0]) // return the first letter of each word of the name
+      .join(''); // join all the letter and return it to the username attribute created
+  });
+};
+
+createUsernames(accounts);
+
+// console.log(accounts); // stw
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+/*
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 //for (const movement of movements) {
 for (const [key, movement] of movements.entries()) {
@@ -99,7 +147,7 @@ movements.forEach(function (movement, index, array) {
 //WORKING WITH ARRAYS
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
-/*
+
 //SLICE does NOT mutate the array
 console.log(arr.slice(2));
 console.log(arr.slice(2, 4));
@@ -130,7 +178,7 @@ console.log([...arr, ...arr2]); // same thing
 //JOIN
 console.log(letters.join(' - '));
 
-*/
+
 
 //FOREACH method with Map and Set
 
@@ -145,3 +193,85 @@ console.log(currenciesUnique);
 currenciesUnique.forEach(function (value, _, map) {
   console.log(`${value}: ${value}`);
 });
+
+
+*/
+
+//CODING CHALLENGE #1
+/*
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsOnlyJulia = dogsJulia.slice(1, -2);
+  const bothDogsArray = dogsOnlyJulia.concat(dogsKate);
+  console.log(bothDogsArray);
+
+  bothDogsArray.forEach(function (age, index) {
+    console.log(
+      age <= 3
+        ? `Dog number ${index + 1} is still a puppy🐶`
+        : `"Dogs number ${index + 1} is and adult, and is ${age} years old"`
+    );
+  });
+};
+
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+
+
+
+// LOOPING with MAP method and returning a new array
+const eurotoToUsd = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return Math.trunc(mov * eurotoToUsd);
+});
+
+//Arrow functin way
+//const movementsUSD = movements.map(mov => Math.trunc(mov * eurotoToUsd));
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+
+  // if (mov > 0) {
+  //   return `Movement ${i + 1}: You deposited ${mov}`;
+  // } else {
+  //   return `Movement ${i + 1}: You withdrew ${Math.abs(mov)}`;
+  // }
+);
+
+
+// FILTER method returns an boolean value with the condition that we set
+const deposits = movements.filter(mov => mov > 0);
+console.log(deposits);
+
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
+//regular FOR loop
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) deposits.push(mov);
+
+console.log(movementsDescriptions);
+*/
+
+// console.log(movements);
+
+// //REDUCE accumulator -> SNOWBALL
+// // const balance = movements.reduce(function (acc, cur, i, arr) {
+// //   console.log(`Iteration ${i}: ${acc} + ${cur}`);
+// //   return acc + cur;
+// // }, 0);
+
+// //REDUCE Arrow function
+// const balance = movements.reduce((acc, cur, i, arr) => acc + cur, 0);
+
+// console.log(balance);
+
+// //USING FOR
+// let balance2 = 0;
+// for (const mov of movements) balance2 += mov;
